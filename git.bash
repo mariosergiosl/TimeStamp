78e8cff509fa458c19283b2f86a72d3b1ede6e5c
marioluz <mario.mssl@gmail.com>
2024-08-26 17:44:06 -0300

    Primeiro commit

--- git.bash
+++ git.bash
@@ -18,6 +18,24 @@ if ! git remote get-url origin &> /dev/null; then
   git remote add origin https://gitlab.com/mario_luz/desafio-devops.git
 fi
 
-# Enviar as alterações para o GitLab (forçando o push na primeira vez)
-git push -u origin main --force
+# Prompt for GitLab username
+read -p "Username for 'https://gitlab.com': " username
 
+# Prompt for GitLab PAT
+read -s -p "Password for 'https://${username}@gitlab.com': " pat
+echo  # Add a newline after the password prompt
+
+# Set Git credentials temporarily for this session
+git config --local credential.helper 'store --file=.git/credentials'
+echo "https://${username}:${pat}@gitlab.com" > .git/credentials
+
+
+# Enviar as alterações para o GitLab (forçando o push na primeira vez)
+# git push -u origin main --force
+
+
+# Enviar as alterações para o GitLab (forçando o push na primeira vez)
+git push -u origin main || git push -u origin main -f
+
+# Remove the temporary credentials file
+rm .git/credentials
