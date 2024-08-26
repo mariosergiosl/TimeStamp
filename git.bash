c979b74597739d7eee3c0e12aa88e5366cb22030
marioluz <mario.mssl@gmail.com>
2024-08-26 17:28:38 -0300

    Primeiro commit

--- git.bash
+++ git.bash
@@ -1,6 +1,23 @@
 #!/bin/bash
-git init
+
+# Verificar se a branch 'main' existe
+if ! git show-ref --verify --quiet refs/heads/main; then
+  # Se não existir, criar a branch 'main'
+  git checkout -b main
+fi
+
+# Adicionar os arquivos ao repositório
 git add .
+
+# Fazer o commit
 git commit -m "Primeiro commit"
-git remote add origin https://gitlab.com/mario_luz/desafio-devops.git
-git push -u origin main
+
+# Verificar se o remote 'origin' já existe
+if ! git remote get-url origin &> /dev/null; then
+  # Se não existir, adicionar o remote
+  git remote add origin https://gitlab.com/mario_luz/desafio-devops.git
+fi
+
+# Enviar as alterações para o GitLab (forçando o push na primeira vez)
+git push -u origin main --force
+
