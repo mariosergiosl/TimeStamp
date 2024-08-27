d12159d9dbdf15a9aed14da9e3ef72b341f9d720
marioluz <mario.mssl@gmail.com>
2024-08-27 17:19:20 -0300

    Primeiro commit

--- scripts/cassandra-install.bash
+++ scripts/cassandra-install.bash
@@ -1,7 +1,17 @@
 #!/bin/bash
-helm repo add k8ssandra https://helm.k8ssandra.io/
+helm repo add k8ssandra https://helm.k8ssandra.io/stable
 helm repo update
+
+
+
+
 helm install k8ssandra k8ssandra/k8ssandra-operator --namespace k8ssandra-operator --create-namespace 
-kubectl apply -f projeto/database/cassandra-datacenter.yaml
+
+# testar o operador:
+# kubectl wait --for=condition=Ready pods --all -n k8ssandra-operator --timeout=60s
+
+kubectl create namespace cassandra
+
+kubectl apply -f database/cassandra-datacenter.yaml -n cassandra
 kubectl get cassandradatacenter -n default
 kubectl get pods -n default
