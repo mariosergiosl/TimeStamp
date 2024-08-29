82b7d82007c9a295b9d4351fbb146d47ed6a8a47
marioluz <mario.mssl@gmail.com>
2024-08-29 11:22:10 -0300

    Primeiro commit

--- scripts/argo-cd_install.bash
+++ scripts/argo-cd_install.bash
@@ -1,3 +1,18 @@
 #/bin/bash
-helm repo add argo https://argoproj.github.io/argo-helm
-helm install argocd argo/argo-cd
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+# via HELM
+# helm repo add argo https://argoproj.argo-cd.github.io/argo-helm
+# helm repo update
+# helm install argocd argo/argo-cd -n argocd
+
+
+# obter a senha inicial
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+
+r5Iw6aHNWOiShwG9
+
+
+# Teste
+kubectl get svc argocd-server -n argocd
