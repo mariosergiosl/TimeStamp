59e76042bc1a8d4af0a5a6d5cb8db17ade90edf9
marioluz <mario.mssl@gmail.com>
2024-08-30 20:11:35 -0300

    Update

--- scripts/criar-aplicacao-argocd.bash
+++ scripts/criar-aplicacao-argocd.bash
@@ -23,6 +23,33 @@ curl -X POST -H "Content-Type: application/json" \
   }' \
   http://192.168.1.4:30080/api/v1/applications
 
-  kubectl apply -f argocd/devops-server_app.yaml
-  kubectl apply -f devops-server_service.yaml -n default
-kubectl apply -f devops-server_ingress.yaml -n default
+kubectl create namespace devops-server
+kubectl apply -f argocd/devops-server_app.yaml -n devops-server
+kubectl apply -f argocd/devops-server_service.yaml -n devops-server
+kubectl apply -f argocd/devops-server_ingress.yaml -n devops-server
+
+
+
+kubectl get services --all-namespaces -o jsonpath='{range .items[*]}{.metadata.name}{"\t"}{.spec.type}{"\t"}{range .spec.ports[*]}{.nodePort}{" "}{end}{"\n"}{end}'
+
+
+# sync da aplicação
+#   pegar token
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+
+#  sync com tokem
+#curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer <seu-token>" http://192.168.1.4:30080/api/v1/applications/devops-server/sync
+
+curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer sZOGbalvHyFswFCpr" http://192.168.1.4:30080/api/v1/applications/devops-server/sync
+
+sZOGbalvHyFswFCpr
+
+
+curl -X POST -H "Content-Type: application/json" -H "Authorization: Basic YWRtaW46c1pPR2JhbHZIeUZzd0ZDcA==" http://192.168.1.4:30080/api/v1/applications/devops-server/sync
+
+
+kubectl get applications -n argocd
+
+
+helm install devops-server ./charts/devops-server -n default
+helm install devops-server ./charts/devops-server -n default --set image.tag=latest
