94d0f2991e0ddf35ecc6b9626cf5576f25022071
marioluz <mario.mssl@gmail.com>
2024-08-30 08:27:15 -0300

    Update

--- scripts/argo-cd_install.bash
+++ scripts/argo-cd_install.bash
@@ -2,39 +2,25 @@
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-
+kubectl apply -f argocd/argocd-service.yaml
 kubectl apply -f argocd/argocd-application.yaml
 kubectl apply -f argocd/argocd-ingress.yaml
-
-# via HELM
-# helm repo add argo https://argoproj.argo-cd.github.io/argo-helm
-# helm repo update
-# helm install argocd argo/argo-cd -n argocd
-
 
 # obter a senha inicial
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 
-r5Iw6aHNWOiShwG9
 
+#mudar o type em ports de ClusterIP para NodePort
+#kubectl edit svc argocd-server -n argocd
 
-
-
-
-echo "192.168.1.4 argocd.desafio" >> /etc/hosts
-
-kubectl edit svc argocd-server -n argocd
-mudar o type em ports de ClusterIP para NodePort
-
-kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "NodePort", "ports": [{"port": 80, "targetPort": 8080, "nodePort": 30080}, {"port": 443, "targetPort": 8080, "nodePort": 30443}]}}'
+#kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "NodePort", "ports": [{"port": 80, "targetPort": 8080, "nodePort": 30080}, {"port": 443, "targetPort": 8080, "nodePort": 30443}]}}'
 
 
 
 # Teste
-kubectl get svc argocd-server -n argocd
-kubectl get all -n argocd
+# kubectl get svc argocd-server -n argocd
+# kubectl get all -n argocd
 
 # Limpeza
-kubectl delete -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-helm uninstall argocd -n argocd
-kubectl delete namespace argocd
+# kubectl delete -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+# kubectl delete namespace argocd
