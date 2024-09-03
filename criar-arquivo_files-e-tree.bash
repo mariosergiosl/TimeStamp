d0bfeafe1b580000861376538eac6b73e6b8a1ac
marioluz <mario.mssl@gmail.com>
2024-09-03 09:45:24 -0300

    Update

--- criar-arquivo_files-e-tree.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Faz backup do arquivo existente
-mv ../files.txt ../files.txt_$(date +"%Y%m%d_%H%M%S") 
-
-# Gera o novo arquivo files.txt
-find . -mindepth 2 -maxdepth 2 -type f ! -path '*/.git/*' -print0 | 
-xargs -0 -I {} sh -c 'echo -e "\n# {} início =============================\n"; cat {}; echo -e "\n# {} fim =============================\n";' >> ../files.txt
